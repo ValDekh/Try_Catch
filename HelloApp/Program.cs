f0be5b4fe7fcb944af9c32a2f37f3a6fc3edc90c
[Your <[Your>
2021-10-07 18:02:39 +0300

    New one

---
+++ HelloApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Catch in Main : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Main");
+            }
+            Console.WriteLine("final block main");
+        }
+    }
+
+    class TestClass
+    {
+        public static void Method1()
+        {
+            try
+            {
+                Method2();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"catch в Method1 : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Method1");
+            }
+            Console.WriteLine("Конец метода Method1");
+        }
+
+        static void Method2()
+        {
+            try
+            {
+                int x = 8;
+                int y = x / 0;
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Mathod2");
+            }
+            Console.WriteLine("Конец метода Method2");
+        }
+    }
+}
---
+++ Try_Catch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Try_Catch
+{
+  
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person p = new Person {Name = "Tom", Age = 17};
+            }
+            catch (PersonalException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Console.WriteLine($"Некорректное значение: {ex.Value} ");
+            }
+           
+        }
+    }
+
+    class PersonalException : ArgumentException
+    {
+        public int Value { get; }
+
+        public PersonalException(string message, int val) : base(message)
+        {
+            Value = val;
+        }
+    }
+
+    class Person
+    {
+        private int age;
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new PersonalException("Нету 18", value);
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+    }
+}
